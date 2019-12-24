9e76a49c99b26f7bae04f351aac8c2419493f6d8
Catherine Smiley <catherinesmiley@gmail.com>
2019-12-24 19:01:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,5 +12,33 @@ def not_safe?(speed)
 	speed > 60 || speed < 40 ? true : false
 end
 	
+	
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def unsafe?(speed)
+#   if speed < 40 || speed > 60
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+# def not_safe?(speed)
+#   speed < 40 || speed > 60 ? true : false 
+# end
